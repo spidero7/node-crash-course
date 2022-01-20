1aade683309b3b8e520873c66db42b9bb25f0722
spidero7 <pajak.bartlomiej97@gmail.com>
2022-01-20 08:56:58 +0100

    Create class Person

---
+++ person.js
@@ -0,0 +1,11 @@
+export class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I'm ${this.age} years old`);
+    }
+}
+
